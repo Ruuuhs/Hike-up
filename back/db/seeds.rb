84266675b8d27835df66652c7152e00432c28165
uryo <t150121@mail.ryukoku.ac.jp>
2020-08-04 16:21:12 +0900

    first commit;

---
+++ back/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+
+User.create!(name: 'Example User',
+            email: 'example@railstutorial.org',
+            # password: 'foobar',
+            # password_confirmation: 'foobar',
+            # profile_image: 'test.png',
+            # admin: true,
+            # activated: true,
+            # activated_at: Time.zone.now
+            )
