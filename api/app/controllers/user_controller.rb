5348b3ba1da6500e31388a57928101a65297e69f
uryo <t150121@mail.ryukoku.ac.jp>
2020-08-06 15:01:14 +0900

    add user controller'

---
+++ api/app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  # GET /users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /users/1
+  def show
+    render json: @user
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+end
