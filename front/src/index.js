d3ba285b99759c6cac2e02089bac31af922386c9
uryo <t150121@mail.ryukoku.ac.jp>
2020-08-17 17:33:10 +0900

    add react-router-dom

--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import SignIn from "./SignIn";
+
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route exact path="/signin" component={SignIn} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
