c477c437a6012fa361bf47d21f41679d0ec72bfe
uryo <t150121@mail.ryukoku.ac.jp>
2020-08-18 18:26:46 +0900

    add hooks

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,28 @@
-import React from "react";
-import "./App.css";
+import React, { useReducer } from "react";
+import reducer from "./reducers";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-function App() {
-  return <div>Hello!</div>;
-}
+import AppContext from "./contexts/AppContext";
+
+import SignIn from "./components/SignIn";
+import About from "./components/About";
+
+const App = () => {
+  const initialState = {
+    users: [],
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={About} />
+          <Route exact path="/signin" component={SignIn} />
+        </Switch>
+      </BrowserRouter>
+    </AppContext.Provider>
+  );
+};
 
 export default App;
