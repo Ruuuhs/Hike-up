22e119e4618321af33c9e68c4a8a7f2d4c2afea4
uryo <t150121@mail.ryukoku.ac.jp>
2020-09-15 16:02:17 +0900

    add comment model

--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 
